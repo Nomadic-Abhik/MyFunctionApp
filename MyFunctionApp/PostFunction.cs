f63071b3495b79069d4daf7186014a95e7f98e63
Abhik Mitra <abhik.mitra06@gmail.com>
2026-02-07 16:41:46 +0530

    message updated

--- MyFunctionApp/PostFunction.cs
+++ MyFunctionApp/PostFunction.cs
@@ -20,7 +20,7 @@ public class PostFunction
 
         string responseMessage = string.IsNullOrEmpty(name)
             ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello Miss, {name}. This HTTP triggered function executed successfully";
+            : $"Hello Miss, {name}. This HTTP triggered function executed successfully.";
 
         return new OkObjectResult(responseMessage);
 
